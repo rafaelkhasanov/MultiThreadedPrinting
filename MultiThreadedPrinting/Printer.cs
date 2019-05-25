8397b51f1fd75fd3f08628aededa9b134a5713c9
Рафаэль Хасанов <rap1raf@gmail.com>
2019-05-25 13:54:48 +0300

    Add project files.

---
+++ MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    public class Printer
+    {
+        private object threadLock = new object();
+        public void PrintNumbers()
+        {
+            lock (threadLock)
+            {
+                //Вывести информацию о потоке
+                Console.WriteLine($"->{Thread.CurrentThread.Name} is executing PrintNumbers()");
+                //Вывести числа
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    //Приостановить поток на случайный период времени
+                    Random r = new Random();
+                    Thread.Sleep(100 * r.Next(5));
+
+                    Console.Write($"{i}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ MultiThreadedPrinting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Synchronizing Threads\n");
+            Printer p = new Printer();
+            //Создать 10 потоков, которые указывают на один и тот же метод того же самого объекта
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+                {
+                    Name = $"Worker thread #{i}"
+                };
+            }
+
+            foreach (Thread item in threads)
+                item.Start();
+            Console.ReadLine();
+        }
+    }
+}
